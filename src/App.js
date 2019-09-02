ae3c6acf7d3cabe75a8cd35d682a4771c340fc60
Martin Marosi <marvusm.mmi@gmail.com>
2019-09-02 13:09:33 +0200

    Removed unnecessary with router decorator.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Main } from '@redhat-cloud-services/frontend-components';
-import { withRouter } from 'react-router-dom';
 import Routes from './routes';
 import './App.scss';
 
-class App extends Component {
-
-  componentDidMount () {
+const App = () => {
+  useEffect(() => {
     insights.chrome.init();
-  }
+  }, []);
 
-  render () {
-    return (
-      <Main>
-        <Routes childProps={ this.props } />
-      </Main>
-    );
-  }
-}
-
-App.propTypes = {
-  history: PropTypes.object
+  return (
+    <Main>
+      <Routes />
+    </Main>
+  );
 };
 
-export default withRouter(App);
+export default App;
