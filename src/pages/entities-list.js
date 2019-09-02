bd01911b83d995aa445603942ef38b00fd03d9ed
Martin Marosi <marvusm.mmi@gmail.com>
2019-09-02 13:49:48 +0200

    Added table example to entities list.

--- src/pages/entities-list.js
+++ src/pages/entities-list.js
@@ -1,18 +1,85 @@
-import React, { useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  textCenter
+} from '@patternfly/react-table';
+
 import { SET_TITLE } from '../store/action-types/top-toolbar-actions';
+import FilterSelect from '../common/filter-select';
+import FilterToolbarItem from '../common/filter-toolbar-item';
+
+const entitiesOptions = [{
+  value: 'source',
+  label: 'Source'
+}, {
+  value: 'vm',
+  label: 'VM'
+}];
+
+const columns = [
+  { title: 'Repositories' },
+  'Branches',
+  { title: 'Pull requests' },
+  'Workspaces',
+  {
+    title: 'Last Commit',
+    transforms: [ textCenter ],
+    cellTransforms: [ textCenter ]
+  }
+];
+
+const rows = [
+  {
+    cells: [ 'one', 'two', 'three', 'four', 'five' ]
+  },
+  {
+    cells: [
+      {
+        title: <div>one - 2</div>,
+        props: { title: 'hover title', colSpan: 3 }
+      },
+      'four - 2',
+      'five - 2'
+    ]
+  },
+  {
+    cells: [
+      'one - 3',
+      'two - 3',
+      'three - 3',
+      'four - 3',
+      {
+        title: 'five - 3 (not centered)',
+        props: { textCenter: false }
+      }
+    ]
+  }
+];
 
 const EntitiesList = () => {
   const [ data, setData ] = useState([]);
+  const [ entityType, setEntityType ] = useState('vm');
+  const [ filterValue, setFilterValue ] = useState('');
   const dispatch = useDispatch();
   useEffect(() => {
     console.log(data, setData);
     dispatch({ type: SET_TITLE, payload: 'Inventory' });
   }, []);
+  console.log(entityType);
   return (
-    <div>
-      There will be dragons
-    </div>
+    <Fragment>
+      <div className="table-filter pf-u-p-lg">
+        <FilterSelect onChange={ value => setEntityType(value) } value={ entityType } options={ entitiesOptions } />
+        <FilterToolbarItem onFilterChange={ value => setFilterValue(value) } searchValue={ filterValue } />
+      </div>
+      <Table aria-label="entities-list" cells={ columns } rows={ rows }>
+        <TableHeader />
+        <TableBody />
+      </Table>
+    </Fragment>
   );
 };
 
