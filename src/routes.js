0da98a6942964c8a2764eab8f2e545e869e5dc5f
Martin Marosi <marvusm.mmi@gmail.com>
2019-10-03 16:22:16 +0200

    Remove unnecessary block in Routes.

--- src/routes.js
+++ src/routes.js
@@ -11,17 +11,15 @@ const paths = {
   topologyView: '/topology-viewer'
 };
 
-const Routes = () => {
-  return (
-    <Suspense fallback={ <div>Loading</div> }>
-      <Switch>
-        <Route exact path={ paths.index } component={ SourcesList }/>
-        <Route exact path={ paths.topologyView } component={ TopologyView }/>
-        <Route exact path={ paths.detail } component={ EntityDetail } />
-        <Route path="*" component={ SourcesList }/>
-      </Switch>
-    </Suspense>
-  );
-};
+const Routes = () => (
+  <Suspense fallback={ <div>Loading</div> }>
+    <Switch>
+      <Route exact path={ paths.index } component={ SourcesList }/>
+      <Route exact path={ paths.topologyView } component={ TopologyView }/>
+      <Route exact path={ paths.detail } component={ EntityDetail } />
+      <Route path="*" component={ SourcesList }/>
+    </Switch>
+  </Suspense>
+);
 
 export default Routes;
