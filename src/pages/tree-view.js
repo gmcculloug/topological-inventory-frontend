2c457f13ac443da7cc1635907ce2e703d26a427f
Martin Maroši <marvusm.mmi@gmail.com>
2020-06-22 13:17:55 +0200

    Connect tree to the API

--- src/pages/tree-view.js
+++ src/pages/tree-view.js
@@ -1,53 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Tree from '../components/tree';
+import {
+  getSources,
+  getServiceOfferingNodes,
+  getServiceInstanceNodes,
+  getServiceInventories,
+  getServiceInstance,
+  getServicePlans,
+  getServiceOfferings,
+} from '../api/ansible-tower';
+import { UPDATE_NODE, SET_DATA } from '../store/action-types/sources-action-types';
 
-const dummyData = [
-  {
-    title: 'Root 1',
-    id: '1',
-    children: [
-      {
-        title: 'Sub node 1',
-        id: '1-1',
-      },
-      {
-        title: 'Sub node 2',
-        id: '1-2',
-        children: [
-          {
-            title: 'Sub node 2-1',
-            id: '2-1-1',
-          },
-          {
-            title: 'Sub node 2-2',
-            id: '2-1-2',
-          },
-          {
-            title: 'Sub node 2-3',
-            id: '2-1-3',
-          },
-          {
-            title: 'Sub node 2-4',
-            id: '2-1-4',
-          },
-        ],
-      },
-      {
-        title: 'Sub node 3',
-        id: '1-3',
-      },
-      {
-        title: 'Sub node 4',
-        id: '1-4',
-      },
-    ],
-  },
-];
+function createNodeData(node) {
+  if (!node) {
+    return;
+  }
+
+  const copy = {};
+  if (node.subCollections) {
+    node.subCollections.forEach((collection) => {
+      copy.children = [
+        ...(copy.children || []),
+        {
+          id: `sub-collection-${collection.type}`,
+          title: collection.type,
+          children: collection.data.data.map((child) => createNodeData(child)),
+        },
+      ];
+    });
+  }
+
+  return {
+    ...copy,
+    id: node.id,
+    title: node.name || node.id,
+  };
+}
+
+function createTreeData(sources) {
+  if (!sources?.data) {
+    return;
+  }
+
+  return sources.data.map((source) => createNodeData(source));
+}
 
 const TreeView = () => {
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const structure = useSelector(({ sourcesReducer }) => sourcesReducer);
+  useEffect(() => {
+    setLoading(true);
+    getSources()
+      .then((data) => {
+        dispatch({ type: SET_DATA, payload: data });
+        return data.data;
+      })
+      .then((sources) => {
+        const promises = sources.map(({ id }) => {
+          const subCollections = [
+            getServiceOfferings(id).then((data) => dispatch({ type: UPDATE_NODE, id, subCollections: { type: 'service-offerings', data } })),
+            getServicePlans(id).then((data) => dispatch({ type: UPDATE_NODE, id, subCollections: { type: 'service-plans', data } })),
+            getServiceInstance(id).then((data) => dispatch({ type: UPDATE_NODE, id, subCollections: { type: 'service-instances', data } })),
+            getServiceInventories(id).then((data) => dispatch({ type: UPDATE_NODE, id, subCollections: { type: 'service-inventories', data } })),
+            getServiceInstanceNodes(id).then((data) => dispatch({ type: UPDATE_NODE, id, subCollections: { type: 'service-instance-nodes', data } })),
+            getServiceOfferingNodes(id).then((data) => dispatch({ type: UPDATE_NODE, id, subCollections: { type: 'service-offering-nodes', data } })),
+          ];
+          return Promise.all(subCollections).then(() => setLoading(false));
+        });
+        return Promise.all(promises);
+      });
+  }, []);
+  if (loading) {
+    return <div>Loading</div>;
+  }
+
+  const treeData = createTreeData(structure);
   return (
     <div>
-      <Tree data={dummyData} />
+      <Tree data={treeData} />
     </div>
   );
 };
